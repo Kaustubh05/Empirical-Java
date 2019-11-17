9366b9100cc50fc4d0bf7076209ab721e7f5de3b
Kaustubh <kbhaler@iu.edu>
2019-11-17 13:35:33 -0500

    Using scanner to get Array number
    Scanner

---
+++ Code/src/ArrayScanner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayScanner {
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+
+	scanner = new Scanner(System.in);
+	
+	System.out.println("Enter the number of elements you want in the array\n");
+	int[] intArray = new int[scanner.nextInt()];
+	
+	System.out.println("Enter the"+ "elements\n");
+	
+	for (int i =0; i<intArray.length;i++) {
+		intArray[i] = scanner.nextInt();
+	}
+	
+	for(int i =0;i<intArray.length;i++) {
+		System.out.println("Your Array is:"+intArray[i]);
+	}
+	
+	
+	}
+		
+}
